46a0526fa4eb5376e694b6f800f1af23fd44219b
йцу <>
2024-10-03 14:26:24 +0300

    AutoCommit_3 октября 2024 г. 14:26:24_SibNout2023

--- Untitled_9876/Untitled_001-100/main.java
+++ Untitled_9876/Untitled_001-100/main.java
@@ -1,14 +1,13 @@
-//package HelloWorld;
-//import java.util.Scanner;
-
-public class HelloWorldClass {
-
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("C:\\D\\GitHub\\GitHubOll\\HowTo_JAVA\\Краткое руководство\\HelloWorld");
-        System.out.println("HelloWorld");
-        //Scanner reader = new Scanner(System.in); 
-        //int n = reader.nextInt();
-        //reader.close();
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+public class PayBarry {
+    public static void main(String args[]) throws UnsupportedEncodingException
+    { 
+        System.setOut(new PrintStream(System.out, true, "windows-1251"));
+        double checkAmount = 1257.63;
+        System.out.print ("Оплатить по распоряжению привет мир");
+        System.out.print ("Барри Берда ");
+        System.out.print ("$");
+        System.out.println(checkAmount);
     }
 }
