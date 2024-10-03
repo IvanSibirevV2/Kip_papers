1219019d9d8c4acf3b6c42565215927a1b2e92ce
йцу <>
2024-10-03 15:42:14 +0300

    AutoCommit_3 октября 2024 г. 15:42:14_SibNout2023

--- Untitled_9876/Untitled_001-100/main.java
+++ Untitled_9876/Untitled_001-100/main.java
@@ -1,13 +1,10 @@
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-public class PayBarry {
-    public static void main(String args[]) throws UnsupportedEncodingException
-    { 
-        System.setOut(new PrintStream(System.out, true, "windows-1251"));
-        double checkAmount = 1257.63;
-        System.out.print ("Оплатить по распоряжению привет мир");
-        System.out.print ("Барри Берда ");
-        System.out.print ("$");
-        System.out.println(checkAmount);
+//package HelloWorld;
+import java.util.Scanner;
+
+public class HelloWorldClass {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        Scanner reader = new Scanner(System.in);int n = reader.nextInt();System.out.println(n);reader.close();
     }
 }
