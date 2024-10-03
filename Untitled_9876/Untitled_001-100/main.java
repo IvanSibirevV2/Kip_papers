f7d3baba0c779158b009af1aa4bfd58702590a20
йцу <>
2024-10-03 13:26:52 +0300

    AutoCommit_3 октября 2024 г. 13:26:52_SibNout2023

---
+++ Untitled_9876/Untitled_001-100/main.java
@@ -0,0 +1,14 @@
+//package HelloWorld;
+//import java.util.Scanner;
+
+public class HelloWorldClass {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("C:\\D\\GitHub\\GitHubOll\\HowTo_JAVA\\Краткое руководство\\HelloWorld");
+        System.out.println("HelloWorld");
+        //Scanner reader = new Scanner(System.in); 
+        //int n = reader.nextInt();
+        //reader.close();
+    }
+}
